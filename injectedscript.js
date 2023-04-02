9c3a1e01c8c8857d424f7666f98515fa08dad318
Fernando Antelo <fjantelo@gmail.com>
2023-04-01 21:11:38 -0400

    Adds logic to prevent reloading on non-YouTube sites and YouTube Music

--- injectedscript.js
+++ injectedscript.js
@@ -1,20 +1,22 @@
 const currentURL = new String(window.location);
-
-var indexEnd = 0;
-for (let i = 0; i < currentURL.length - 5; i++) {
-  if (
-    currentURL[i] == "&" &&
-    currentURL[i + 1] == "l" &&
-    currentURL[i + 2] == "i" &&
-    currentURL[i + 3] == "s" &&
-    currentURL[i + 4] == "t"
-  ) {
-    indexEnd = i;
-    break;
+var isYoutubeURL = false;
+if (currentURL.includes("youtube.com") && currentURL.includes("&list") && !currentURL.includes("music.youtube.com")) {
+  isYoutubeURL = true;
+}
+if (isYoutubeURL) {
+  var indexEnd = 0;
+  for (let i = 0; i < currentURL.length - 5; i++) {
+    if (
+      currentURL[i] == "&" &&
+      currentURL[i + 1] == "l" &&
+      currentURL[i + 2] == "i" &&
+      currentURL[i + 3] == "s" &&
+      currentURL[i + 4] == "t"
+    ) {
+      indexEnd = i;
+      break;
+    }
   }
+  var newURL = currentURL.substring(0, indexEnd);
+  window.location = newURL;
 }
-var newURL = "";
-for (let i = 0; i < indexEnd; i++) {
-  newURL += currentURL[i];
-}
-window.location = newURL;
